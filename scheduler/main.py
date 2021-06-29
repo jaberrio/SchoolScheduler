e22ad9dcc4f1ce7e3baf5c7736eef1574b708ce7
michaelberry0321 <46459648+michaelberry0321@users.noreply.github.com>
2021-06-28 20:04:31 -0400

    Created a way to upload dependencies to pip. Also changed file structure

---
+++ scheduler/main.py
@@ -0,0 +1,39 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication
+
+from scheduler import schedulerui
+from scheduler.schedule import *
+from scheduler.wsl import *
+
+
+class SchoolScheduler(QtWidgets.QMainWindow, schedulerui.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(SchoolScheduler, self).__init__(parent)
+        self.setupUi(self)
+
+
+
+
+def start_gui():
+    app = QApplication(sys.argv)
+    form = SchoolScheduler()
+    form.show()
+    app.exec_()
+
+
+def main():
+    # UI
+    set_display_to_host()
+    start_gui()
+
+
+if __name__ == '__main__':
+    # purge fixes fresh start up bug
+    # TODO REMOVE COMMENT OUT TO ALLOW PURGE
+    # db_purge()
+
+    db_init()
+    main()
+    db_close()
